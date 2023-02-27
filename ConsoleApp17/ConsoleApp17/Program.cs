e4e4359bc97262b292acfa19b698590f09060583
Наима <113019779+Naimabilal19@users.noreply.github.com>
2023-02-27 14:03:58 +0200

    Add files via upload

---
+++ ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    interface ibill
+    {
+        void Print();
+    }
+    class Card
+    {
+        public string number { get; set; }
+        public int money { get; set; }
+        public int cvv { get; set; }
+
+        public Card() { }
+        public Card(string n, int c, int m)
+        {
+            number = n;
+            cvv = c;
+            money = m;
+        }
+        public void Printmoney()
+        {
+            Console.WriteLine(money);
+        }
+    }
+
+
+    class ATM : Card, ibill
+    {
+        int amount { get; set; }
+
+        public ATM() { }
+        public ATM(int a)
+        {
+            amount = a;
+        }
+        public void Add(Card Obj, int a)
+        {
+            money += a;
+        }
+        public void Take(Card Obj, int a)
+        {
+            money -= a;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Сумма: {0}", money);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card c = new Card("123456", 235000, 123);
+            c.number = "123456";
+            c.money = 23500;
+            c.cvv = 123;
+
+            ATM a = new ATM();
+            a.Print();
+
+        }
+    }
+}
